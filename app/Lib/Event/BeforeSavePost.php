d675941290e216879adf03e70c5fecacb1f0f2b2
MiletEcom <miletza@ecomexperts.com>
2021-06-29 17:05:00 -0300

    finalmente events

--- app/Lib/Event/BeforeSavePost.php
+++ app/Lib/Event/BeforeSavePost.php
@@ -1,38 +1,32 @@
 <?php
-
 App::uses('CakeEventListener', 'Event');
-//App::uses('AppModel', 'Model');
-
+//App::uses('Post', 'Model');
 class BeforeSavePost implements CakeEventListener {
     public function implementedEvents() {
         return array(
-            'Model.Post.beforeSave' => 'updateSlugPost'
+            'Model.Post.beforeSave' => 'updateSlugPost',
+            'passParams' => true
         );
     }
-
     public function updateSlugPost($event) {
         //die('holaaa');
-
-        //debug($event->data['data']['title']);
         $titlePost = strtolower(Inflector::slug($event->data['data']['title'],'-') );
-        debug($titlePost);die();
-        $this->Post = ClassPost::init('Post.beforeSave');
-        
-        //$titlePost=strtolower(str_replace(' ','-',$event->data['title']));
+        $Post = ClassRegistry::init('Post');
+        $datos = $Post->find('all', array(
+            'conditions' => array(
+                'Post.slug' => $titlePost
+                )
+            )
+        );
+        if (empty($datos)) {
+            $event->data['Post']['slug'] = $titlePost;
+            $Post->save($event->data['Post']);
+        }else{
+            $event->data['Post']['slug'] = $titlePost.'-2'; //agrego un -2 al final para que siga siendo unico
+            $Post->save($event->data['Post']);
+        }
 
-        /*$this->Post->id = $event->data['id'];
-        $this->Post->set(array(
-            'slug' => $titlePost
-        ));*/
-        $conditions = array("NOT" => array(
-            "Post.slug" => array($event->data['title'])
-        ) );
-        $this->Post->set('posts', $this->Post->find('all', array('conditions' => $conditions)));
-
-        $this->Post->save();
     }
-
-
 }
 
 ?>
