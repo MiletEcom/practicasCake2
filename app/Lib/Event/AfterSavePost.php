344a18541994a9372a120d42bfe130d2a65bb38e
MiletEcom <miletza@ecomexperts.com>
2021-06-29 14:09:30 -0300

    commit otra configuracion para event

--- app/Lib/Event/AfterSavePost.php
+++ app/Lib/Event/AfterSavePost.php
@@ -3,18 +3,6 @@
 App::uses('CakeEventListener', 'Event');
 
 class AfterSavePost implements CakeEventListener {
-    /**
-     *
-     * @var Document The model.
-     */
-    protected $Document;
-
-    public function __construct()
-    {
-        // referencia al modelo que usaremos
-        $this->Document = ClassPost::init('Post.afterSave');
-    }
-
     public function implementedEvents() {
         return array(
             'Model.Post.afterSave' => 'updateSlugPost',
@@ -23,29 +11,22 @@ class AfterSavePost implements CakeEventListener {
     }
 
     public function updateSlugPost(CakeEvent $event) {
-
-        $data = $event->data;
-
-        debug($data);
-        /*
-        $subject = $event->subject();
-
-        $this->Document->SomethingImportantHappened($data,$subject);
-
-
-        debug($data);
+        debug($event);
         die();
-        $this->Post = ClassEdit::init('Post');
-        $titlePost = strtolower(Inflector::slug($post['Post']['title'],'-') );
-        //$titlePost=strtolower(str_replace(' ','-',$post['Post']['title']));
+        $this->Post = ClassPost::init('Post.afterSave');
+        //$titlePost = strtolower(Inflector::slug($event->data['title'],'-') );
+        $titlePost=strtolower(str_replace(' ','-',$event->data['title']));
 
         $this->Post->id = $event->data['id'];
         $this->Post->set(array(
             'slug' => $titlePost
         ));
         $this->Post->save();
-        */
-        
+
+        /*$data = $event->data;
+        $subject = $event->subject();
+
+        $this->Document->SomethingImportantHappened($data,$subject); */
     }
 }
 
