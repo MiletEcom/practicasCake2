f31ccd036eff89fa81068313c07d788b1884ef6c
MiletEcom <86018850+MiletEcom@users.noreply.github.com>
2021-07-01 12:57:12 -0300

    Update FindPostShell.php
    prueba porque no cambio con el ultimo commit

--- app/Console/Command/FindPostShell.php
+++ app/Console/Command/FindPostShell.php
@@ -2,13 +2,11 @@
 class FindPostShell extends AppShell {
     
     public $uses = array('Post');
-    //public $post = array('Post');
 
     public function main() {
         $this->out('Hola, ingresa con la palabra show');
     }
     public function show() {
-         //die('holaaa');
         $opciones=array(
             'limit' => 10,
             'page' => 1,
@@ -16,9 +14,8 @@ class FindPostShell extends AppShell {
         $posts = $this->Post->find('all',$opciones);
 
         while($posts){
-            //$post = $this->Post->find('all',$opciones);
+            $posts = $this->Post->find('all',$opciones);
             foreach($posts as $p) {
-                
                 //$this->out($p['Post']['slug']);//die();
                 if (empty($p['Post']['slug'])) {
                 
@@ -26,20 +23,18 @@ class FindPostShell extends AppShell {
                     $p['Post']['slug'] = $titlePost;
 
                     if (!$this->Post->save($p['Post'])) {
-                        $this->out('No guardo');
+                        $this->out('FALSE, no guardo');
+                    }else{
+                        //die('holaaa');
+                        $this->out('Le agregué una nueva slug '.$titlePost);
                     }
-                       
-                    // echo debug($this->Post->id);die();
-                    $this->out('Le agregué una nueva slug '.$titlePost);
                      
                 }else{
-       
                    $this->out('Ya el post tiene una slug asociada, no hago nada');
                 }
             }
             
             $opciones['page']++;
         }
-        //die();
     }
 }
